6596a49e2f1c750289436cb5f394cb4901f374ab
w780560603 <150361356+w780560603@users.noreply.github.com>
2026-09-09 02:39:45 +0800

    123

--- toolkit/phone/phone-main-ui.js
+++ toolkit/phone/phone-main-ui.js
@@ -24,6 +24,7 @@ import { showConfirmPopup } from './utils/popup-helper.js';
 import pageStack from './utils/page-stack-helper.js';
 import { getUserDisplayName } from './utils/contact-display-helper.js';
 import { initTheme } from './utils/theme-manager.js';
+import { renderMomentsFeed } from './moments/moments-feed-ui.js';
 
 /**
  * 渲染手机框架
@@ -539,8 +540,6 @@ async function renderMomentsTab(overlayElement) {
   }
 
   try {
-    const { renderMomentsFeed } = await import('./moments/moments-feed-ui.js');
-
     tabContainer.innerHTML = '';
 
     const feed = await renderMomentsFeed();
@@ -558,6 +557,7 @@ async function renderMomentsTab(overlayElement) {
     console.error('[SuST] 动态页面加载失败:', error);
   }
 }
+
 /**
  * 处理加号菜单项点击
  *
